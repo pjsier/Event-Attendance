640c7d90c0016665d04f9bef050e123b88865eef
Alton Michaux <alton.michaux@yahoo.com>
2021-08-16 11:43:58 -0400

    <design>coded some conditional design patterns for the home page concerning the about page and created an event types page for use in mobile view

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   resources :events
 
   get 'pages/about', to: 'pages#about'
+  get 'pages/types', to: 'pages#types'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
