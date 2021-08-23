184fcb8b619496b36f8f1b5a0580b9eaf170a00e
gsyoung2020 <gsyoung2019@gmail.com>
2021-08-23 15:20:18 -0400

    the mailer is working but not sending, will work on it

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   root to: "pages#index"
   resources :members
   resources :events
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  # Some setup you must do manually if you haven't yet:
+
+  #   Ensure you have overridden routes for generated controllers in your routes.rb.
+  #   For example:
+
+  #     Rails.application.routes.draw do
+  #       devise_for :users, controllers: {
+  #         sessions: 'users/sessions'
+  #       }
+  #     end
+
 end
