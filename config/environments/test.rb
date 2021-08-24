d8d91c2641f1f235c7c2e91e872f68a75ea97db1
gsyoung2020 <gsyoung2019@gmail.com>
2021-08-23 20:54:05 -0400

    tried changing the settings on the gmail set up

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
+    :address              => "smtp-relay.gmail.com",
     :port                 => 587,
     :user_name            => 'eattendace@gmail.com',
     :password             => 'eivqkewzeycqqhwg',
